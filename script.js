63b1b803db2a1abe991cef3d3a478623769e7f81
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-08-03 12:10:09 +0500

    Added map with Leaflet library

--- script.js
+++ script.js
@@ -15,6 +15,19 @@ if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(function(position){
     const {latitude, longitude} = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude},12z`)
+
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
   }, function(){
     alert('Could not get your position')
   })
